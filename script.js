26fb184317b76bc6124c899ac104487716aba3e5
Tom Bijesse <tom@bijesse.com>
2016-08-05 09:46:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function checkCollision() {
+    var blueLeft = $("#blue").offset().left;
+    var redLeft = $("#red").offset().left;
+    var blueRight = blueLeft + $("#blue").width();
+    var redRight = redLeft + $("#red").width();
+    if(blueRight > redLeft && blueLeft < redRight) {
+        alert("Touching");
+    }
+}
+
+$("body").keydown(function(event) {
+    //moves left
+    if (event.which === 37) {
+        $("#blue").css("left", $("#blue").offset().left - 10);
+    //moves right
+    } else if (event.which === 39) {
+        $("#blue").css("left", $("#blue").offset().left + 10);
+    } else {
+        return;
+    }
+    
+        //Checks for position relative to each other
+        checkCollision();
+});
+
